4e1461334076ec84939fad2a97138689361e50e1
Rahsan Al Saymon <115401494+rahsanalsaymon@users.noreply.github.com>
2022-11-17 18:57:30 +0600

    Create 318A Even Odds.cpp

---
+++ 318A Even Odds.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n, k;
+    cin>>n>>k;
+
+    if(n % 2 == 0){
+        if(k <= n/2){
+            cout<<(k * 2) - 1<<endl;
+        }
+        else{
+            cout<<(k - ((n+1)/2))* 2<<endl;
+        }
+    }
+    else{
+        if(k <= (n+1)/2){
+            cout<<(k * 2) - 1<<endl;
+        }
+        else{
+            cout<<(k - ((n+1)/2))* 2<<endl;
+        }
+    }
+
+}
