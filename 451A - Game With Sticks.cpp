42c2f468a7db59fd37c0dd02d62ccfb0724f0407
Rahsan Al Saymon <115401494+rahsanalsaymon@users.noreply.github.com>
2023-03-18 14:51:29 +0600

    Create 451A - Game With Sticks.cpp

---
+++ 451A - Game With Sticks.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin>>n>>m;
+    if(min(n, m) % 2 == 0){
+        cout<<"Malvika\n";
+    }
+    else{
+        cout<<"Akshat\n";
+    }
+}
